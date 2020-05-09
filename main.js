9899e34c91bce328a59edb755c2fabd1d607032d
astronomyf <47946866+astronomyf@users.noreply.github.com>
2020-05-09 21:53:35 +0200

    Aggiornato il main
    Inserite le cose da aggiungere nel main, ora funziona il set interval principale. Tutte le funzioni utili create dovranno essere richiamate qui.

--- main.js
+++ main.js
@@ -1,61 +1,38 @@
-var ProductsName = ['Cheese', 'Banana', 'Apple', 'Rice', 'Meat', 'Nutella', 'Green Beans', 'Salad', 'Roast Beef', 'Dried Fruit', 'Pasta','Soap','Toilet Paper','Hamburger','Water','Coca-Cola','Chicken Wings'];
-var id = 0;
+/**
+ * @file main.js
+ * @author Francesco Violante
+ * 
+ * Main file for our project.
+ */
 
-function randomDate(date1, date2){
-    function randomValueBetween(min, max) {
-      return Math.random() * (max - min) + min;
-    }
-    var date1 = date1 || '01-01-1970';
-    var date2 = date2 || new Date().toLocaleDateString();
-    date1 = new Date(date1).getTime();
-    date2 = new Date(date2).getTime();
-    if( date1>date2){
-        return new Date(randomValueBetween(date2,date1)).toLocaleDateString();  
-    } else{
-        return new Date(randomValueBetween(date1, date2)).toLocaleDateString();  
+ var duration = configSettings.numberOfSeconds * 1000;
+ var weekCounter = 0;
+ var listaSettimanale = [];
 
+ function runProgram() {
+    // qui si richiamano le funzioni principali e si stampa la lista
+    for(var i = 0; i < configSettings.numberOfNewProducts; i++) {
+        var prodotto = generateProduct();
+        listaSettimanale.push(prodotto);
     }
-}
 
-function IdInitializer(i)
-{
-    if(i.toString().length == 1)
-    {
-        i = "00" + i;
+    if(weekCounter != 0) { // se è la prima settimana non vanno eseguiti check
+        // esegui i check
     }
-    else
-    {
-        i = "0" + i;
-    }
-    return i;
-}
 
-//classe per definire gli oggetti del negozio
-class Product
-{
-    constructor(id, name, status, data, price, weight)
-    {
-        this.id = id; //parametro dell'id
-        this.name = name; //parametro del nome
-        this.status = status; //parametro dello status
-        this.data = data; //parametro della data
-        this.price = price; //parametro del costo
-        this.weight = weight; //parametro del peso
-    }
-}
+    // rimuovere prodotti scaduti che sono presenti da almeno una settimana
+    // aggiornare gli stati dei prodotti
 
-var rn = 0; //variabile per i nomi random
-var rs = 0; //variabile per gli status random
-var ps = 0; //variabile per il peso random
-var cs = 0; //variabile per il costo random
-var p = []; //array vuoto per i prodotti
-for(var j = 0; j < 30; j++)
-{
-    id = IdInitializer(id);
-    rn = Math.floor(Math.random() * ProductsName.length); //i diventa un intero casuale tra 0 e la lunghezza dell'array dei nomi dei prodotti
-    cs = Math.floor(Math.random() * (10.01 - 1.01) + 1.01); //funzione per il costo random tra 0 e 500
-    ps = Math.floor(Math.random() * (10.01 - 1.01) + 1.01); //funzione per il peso random tra 0 e 500
-    p[j] = new Product(id, ProductsName[rn], '', randomDate('01-01-2020'), cs + "$", ps + "g");
-    id++;
-    console.log(p[j]);
-}
+    // stampare lista settimanale
+
+    // stampare lista dei prodotti filtrata
+
+    // dopo tutte le operazioni incrementiamo il counter del num di settimane
+    weekCounter++;
+    if(weekCounter >= configSettings.numberOfWeeks) {
+        clearInterval(weekIntervalId);
+    }
+ }
+
+ //print and update the list each week
+ var weekIntervalId = setInterval(runProgram, duration);
