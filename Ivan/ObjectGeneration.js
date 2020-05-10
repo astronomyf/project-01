914d63dc3279e6c21930770985427624df2e8a55
Imeo99 <64216276+Imeo99@users.noreply.github.com>
2020-05-10 17:01:06 +0200

    Update ObjectGeneration.js

--- Ivan/ObjectGeneration.js
+++ Ivan/ObjectGeneration.js
@@ -14,16 +14,14 @@ var ProductsName = ['Cheese', 'Banana', 'Apple', 'Rice', 'Meat', 'Nutella', 'Gre
 //variable used for setting a product's id
 var id = 0;
 
-function randomDate(start, end) 
-{
+function randomDate(start, end) {
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toLocaleDateString();
 }
   
 console.log(randomDate(new Date(2020, 01, 01), new Date(2020, 05, 10)));
 
 //class used for creating the products
-class Product
-{
+class Product {
     constructor(id, name, status, data, price, weight)
     {
         this.id = id;
